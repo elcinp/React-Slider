5014e83b7b6b16a645bcde4a5cd4693fe604e611
elcinp <elcinpars1@gmail.com>
2022-02-09 21:57:40 +0300

    button right onClick function added

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
         <button className="prev" onClick={()=> setIndex(index+1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={()=>setIndex(index - 1)} >
           <FiChevronRight />
         </button>
       </div>
