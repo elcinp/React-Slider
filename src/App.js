c301c368c6cb4147898df056ffc8c70a36fc130a
elcinp <elcinpars1@gmail.com>
2022-02-09 22:04:07 +0300

    useEffect added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
+  useEffect(()=>{
+    const lastIndex = people.length -1
+    if(index<0){
+      setIndex(lastIndex)
+    }
+    if(index>lastIndex){
+      setIndex(0)
+    }
+  },[index,people])
+
   return (
     <section className="section">
       <div className="title">
