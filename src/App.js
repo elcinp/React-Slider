cfa48918986d99d7ccf442c9a863aa7cc8059823
elcinp <elcinpars1@gmail.com>
2022-02-09 21:52:51 +0300

    next last active slide added

--- src/App.js
+++ src/App.js
@@ -19,8 +19,15 @@ function App() {
       {people.map((person,personIndex) => {
         const {id,image,name,title,quote} = person
         //more stuff  xcoming up
+        let position ='nextSlide'
+        if(personIndex === index){
+          position = 'activeSlide'
+        }
+        if(personIndex===index-1 || (index === 0 && personIndex=== people.length - 1)) {
+          position ='lastSlide'
+        }
         return (
-          <article key={id}>
+          <article className={position} key={id}>
             <img src={image} alt="" className='person-img' />
             <h4>{name}</h4>
             <p className='title'>{title}</p>
